79213a00b1df69ef75e65d607b44723a52d612b9
William <mhf.eng.dsg@gmail.com>
2022-08-24 10:28:42 -0300

    Upload Rocketnotes Front-End

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export function Button({title, loading = false, ...rest}) {
+ return(
+    <Container type="button"
+    {...rest}
+    disabled={loading}>
+    {loading ? 'Carregando...' : title  }
+    </Container>
+ )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
+import { useAuth} from '../../hooks/auth'
+import avatarPlaceHolder from '../../assets/avatar_placeholder.svg'
+import {api} from '../../services/api'
+
+export function Header() {
+    const {signOut, user} = useAuth()
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceHolder
+
+
+    return(
+        <Container>
+            <Profile to="/profile">
+                <img src={avatarUrl} alt={user.name} />
+
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>{user.name}</strong>
+                </div>
+            </Profile>
+
+            <Logout onClick={signOut}>
+                <RiShutDownLine />
+            </Logout>
+        </Container>
+    )
+}
